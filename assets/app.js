9cd424b3281313c60c7ef283ebc3aa79b2cfee59
Marcus Whitmore <marcus.whitmore@gmail.com>
2019-04-17 16:46:21 -0500

    html/css changes, add comments in app.js for google map api

--- assets/app.js
+++ assets/app.js
@@ -26,3 +26,14 @@ $.ajax({
 
   });
 
+// this may be helpful for adding the map
+  // https://developers.google.com/maps/documentation/javascript/adding-a-google-map
+  // function initMap() {
+  //   // The location of Uluru
+  //   var uluru = {lat: -25.344, lng: 131.036};
+  //   // The map, centered at Uluru
+  //   var map = new google.maps.Map(
+  //       document.getElementById('map'), {zoom: 4, center: uluru});
+  //   // The marker, positioned at Uluru
+  //   var marker = new google.maps.Marker({position: uluru, map: map});
+  // }
